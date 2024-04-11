a6e8d9629988c5e1835bba8dbc320e2b9ede4de8
Hunar Batra <i@hunarbatra.com>
2024-04-11 16:05:00 +0530

    edit trainer.py

--- trainer.py
+++ trainer.py
@@ -1,14 +1,16 @@
 import torch
 import torch.nn as nn
 
-def train_mle(net, train_loader, test_loader, device, epochs):
-    net.to(device)
+from utils import DEVICE
+
+def train_mle(net, train_loader, test_loader, epochs):
+    net.to(DEVICE)
     optimizer = torch.optim.Adam(net.parameters(), lr=1e-3)
     criterion = nn.CrossEntropyLoss()
 
     for epoch in range(epochs):
         for inputs, targets in train_loader:
-            inputs, targets = inputs.to(device), targets.to(device)
+            inputs, targets = inputs.to(DEVICE), targets.to(DEVICE)
             optimizer.zero_grad()
             outputs = net(inputs)
             loss = criterion(outputs, targets)
@@ -20,7 +22,7 @@ def train_mle(net, train_loader, test_loader, device, epochs):
     total = 0
     with torch.no_grad():
         for inputs, targets in test_loader:
-            inputs, targets = inputs.to(device), targets.to(device)
+            inputs, targets = inputs.to(DEVICE), targets.to(DEVICE)
             outputs = net(inputs)
             _, predicted = torch.max(outputs.data, 1)
             total += targets.size(0)
