adb59ec886b95a94e5abae10bf3511c94ec5bb5b
Hunar Batra <i@hunarbatra.com>
2024-04-11 16:06:37 +0530

    add bnn coreset finetuning script

---
+++ finetune.py
@@ -0,0 +1,14 @@
+import torch
+import tyxe
+
+from utils import DEVICE    
+        
+        
+def finetune_over_coreset(bnn_coreset, curr_coreset, num_epochs, callback, batch_size):
+    # finetune the model over the coreset data point
+    print('$$$$$$ finetuning bnn_coreset $$$$$$')
+    coreset_loader = torch.utils.data.DataLoader(curr_coreset, batch_size=batch_size, shuffle=True)
+    optim = pyro.optim.Adam({"lr": 1e-3})
+    
+    with tyxe.poutine.local_reparameterization():
+        bnn_coreset.fit(coreset_loader, optim, num_epochs, device=DEVICE, callback=callback)
