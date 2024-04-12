d676c00bddcdcdd13f323db97d05dbfd9fc9760a
Hunar Batra <i@hunarbatra.com>
2024-04-12 18:48:55 +0530

    add finetune_method

--- finetune.py
+++ finetune.py
@@ -5,11 +5,14 @@ import pyro
 from utils import DEVICE    
         
         
-def finetune_over_coreset(bnn_coreset, curr_coreset, num_epochs, callback, batch_size):
+def finetune_over_coreset(bnn_coreset, curr_coreset, num_epochs, callback=None, batch_size=256, finetune_method=None):
     # finetune the model over the coreset data point
     print('$$$$$$ finetuning bnn_coreset $$$$$$')
     coreset_loader = torch.utils.data.DataLoader(curr_coreset, batch_size=batch_size, shuffle=True)
     optim = pyro.optim.Adam({"lr": 1e-3})
     
     with tyxe.poutine.local_reparameterization():
-        bnn_coreset.fit(coreset_loader, optim, num_epochs, device=DEVICE, callback=callback)
+        if callback is None:
+            bnn_coreset.fit(coreset_loader, optim, num_epochs, device=DEVICE)
+        else:
+            bnn_coreset.fit(coreset_loader, optim, num_epochs, device=DEVICE, callback=callback)
