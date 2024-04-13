1145279a62f454d1b5c0015e50b7004b012527ae
Hunar Batra <i@hunarbatra.com>
2024-04-13 05:50:00 +0530

    add test scripts

---
+++ run_scripts/run2_1.sh
@@ -0,0 +1,23 @@
+######## PERMUTED MNIST #########
+# VCL
+python3 run_vcl.py --num_tasks=5 --num_epochs=100 --task_config='permuted_mnist' --coreset_size=0 --coreset_method=None --experiment_name='run2' &&
+# VCL + Random Coreset + 200
+python3 run_vcl.py --num_tasks=5 --num_epochs=100 --task_config='permuted_mnist' --coreset_size=200 --coreset_method='random' --experiment_name='run2' &&
+# VCL + K-Center Coreset + 200
+python3 run_vcl.py --num_tasks=5 --num_epochs=100 --task_config='permuted_mnist' --coreset_size=200 --coreset_method='k-center' --experiment_name='run2' &&
+# VCL + PCA-K-Center Coreset + 200
+python3 run_vcl.py --num_tasks=5 --num_epochs=100 --task_config='permuted_mnist' --coreset_size=200 --coreset_method='k-center' --experiment_name='run2' &&
+# VCL+EWC
+python3 run_vcl_ewc.py --num_tasks=5 --num_epochs=100 --task_config='permuted_mnist' --coreset_size=0 --coreset_method=None --experiment_name='run2' --ewc_lambda=100 &&
+# VCL+EWC + Random Coreset + 200
+python3 run_vcl_ewc.py --num_tasks=5 --num_epochs=100 --task_config='permuted_mnist' --coreset_size=200 --coreset_method='random' --experiment_name='run2' --ewc_lambda=100 &&
+# VCL+EWC + K-Center Coreset + 200
+python3 run_vcl_ewc.py --num_tasks=5 --num_epochs=100 --task_config='permuted_mnist' --coreset_size=200 --coreset_method='k-center' --experiment_name='run2' --ewc_lambda=100 &&
+# VCL+EWC + PCA-K-Center Coreset + 200
+python3 run_vcl_ewc.py --num_tasks=5 --num_epochs=100 --task_config='permuted_mnist' --coreset_size=200 --coreset_method='pca-k-center' --experiment_name='run2' --ewc_lambda=100 &&
+# Coreset only + Random Coreset + 200
+python3 coreset.py --num_tasks=5 --num_epochs=100 --task_config='permuted_mnist' --coreset_size=200 --coreset_method='random' --experiment_name='run2' &&
+# Coreset only + K-Center Coreset + 200
+python3 coreset.py --num_tasks=5 --num_epochs=100 --task_config='permuted_mnist' --coreset_size=200 --coreset_method='k-center' --experiment_name='run2' &&
+# Coreset only + PCA-K-Center Coreset + 200
+python3 coreset.py --num_tasks=5 --num_epochs=100 --task_config='permuted_mnist' --coreset_size=200 --coreset_method='pca-k-center' --experiment_name='run2'
