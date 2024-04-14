5e86038703487d9c0843d43af9646ecb0fcb7fde
Hunar Batra <i@hunarbatra.com>
2024-04-15 02:10:22 +0530

    fix model_name

--- run_ewc.py
+++ run_ewc.py
@@ -182,7 +182,7 @@ def run_ewc(
             prev_task_acc.append(accuracy)
         
         avg_acc = sum(prev_task_acc) / len(prev_task_acc)
-        save_results(get_model_name('evcl', model_suffix=model_suffix), j, prev_task_acc, avg_acc, data_name, experiment_name)
+        save_results(get_model_name('ewc', model_suffix=model_suffix), j, prev_task_acc, avg_acc, data_name, experiment_name)
         
         print(f"Train over task {i} avg: {avg_acc}")
         
