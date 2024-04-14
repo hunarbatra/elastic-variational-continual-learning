e0b72dface87148dd8200b4f14c3fae94347a037
Hunar Batra <i@hunarbatra.com>
2024-04-14 21:40:54 +0530

    add plots script

--- utils/plots.py
+++ utils/plots.py
@@ -1,24 +1,80 @@
-# import matplotlib.pyplot as plt
-# import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
 
-# def plot_results(results_dict, filename, title=''):
-#     plt.figure(figsize=(7, 3))
-#     for name, results in results_dict.items():
-#         # avg_results = []
-#         # for i in range(len(results)):
-#         #     avg_result = np.mean(results[:i+1])
-#         #     avg_results.append(avg_result)
-#         plt.plot(np.arange(1, 1+len(results)), results, label=name, marker='o')
+import random
+import fire
 
-#         # Print accuracy values on top of the dots
-#         for i, res in enumerate(results):
-#             plt.text(i+1, res, f'{res:.2f}', ha='center', va='bottom')
+from model_names_mapper import MODEL_NAMES_MAPPER
 
-#     plt.xticks(np.arange(1, len(results) + 1))
-#     plt.xlabel('Task #')
-#     plt.ylabel('Average Accuracy')
-#     plt.title(title)
-#     plt.legend()
-#     plt.tight_layout()
-#     plt.savefig(f'{filename}.png')
-#     plt.show()
+
+def map_model_names(model_data):
+    model_data = {MODEL_NAMES_MAPPER[key]: value for key, value in model_data.items()}
+    
+    return model_data
+
+def load_data(task_name, file_name, remove_models, experiments_dir):
+    df = pd.read_csv(f'./{experiments_dir}/{task_name}/{file_name}.csv')
+    if len(remove_models) > 0:
+        df = df[~df['model'].isin(remove_models)]
+    model_data = {}
+    for model_name, group in df.groupby('model'):
+        accuracies = group['avg_acc'].tolist()
+        model_data[model_name] = accuracies
+    
+    sorted_models = sorted(model_data.keys(), key=lambda x: (x.startswith('evcl') or x.startswith('vcl_ewc'), x.startswith('vcl'), x.startswith('ewc'), x.startswith('coreset'), len(x)))
+    sorted_model_data = {model: model_data[model] for model in sorted_models}
+    
+    model_data = map_model_names(sorted_model_data)
+    
+    return model_data
+
+def plot_avg_accuracy(task_name, file_name, save_name=None, title='', ylim_low=None, remove_models=[], experiments_dir='./experiments'):
+    model_data = load_data(task_name, file_name, remove_models, experiments_dir)
+    
+    plt.figure(figsize=(10, 4), dpi=100)
+    # plt.style.use(['science', 'ieee'])
+
+    for model_name, accuracies in model_data.items():
+        line_style = '-'
+        marker = 'o'
+        if model_name.startswith('EWC'):
+            line_style = '--'
+            marker = '*'
+        elif model_name.startswith('EVCL'):
+            if 'Coreset' in model_name:
+                line_style = '--'
+                marker = 's'
+            else:
+                line_style = '-'
+                marker = 's'
+        elif model_name.startswith('VCL'):
+            if 'Coreset' in model_name:
+                line_style = '--'
+                marker = 'o'
+            else:
+                line_style = '-'
+                marker = 'o'
+        elif 'Only' in model_name:
+            line_style = '--'
+            marker = 'p'
+
+        x = range(1, len(accuracies) + 1)
+        plt.plot(x, accuracies, linestyle=line_style, marker=marker, label=model_name)
+
+    plt.xlabel('# tasks', fontsize=10)
+    plt.ylabel('Average Accuracy', fontsize=10)
+    if len(title) > 0:
+        plt.title(title, fontsize=12)
+    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5), fontsize=10)
+    plt.xticks(range(1, len(x) + 1))  
+    if ylim_low is not None:
+        plt.ylim(ylim_low, 1.0)
+    plt.tight_layout()
+    if save_name is not None:
+        plt.savefig(f'{experiments_dir}/{task_name}/{save_name}.pdf')
+    plt.show()
+        
+    
+if __name__ == '__main__':
+    fire.Fire(plot_avg_accuracy)
